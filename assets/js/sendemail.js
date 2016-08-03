4aafe5401426a5725e66c93524a9371e4c5a0202
Marcus Beckenkamp <mvbeck@gmail.com>
2016-08-03 18:40:27 -0300

    Mensagens de erro e sucesso no envio do email

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,5 +1,14 @@
 $('form').submit(function(event) {
-    console.log($( this ).serializeArray());
-    $.post("../sendemail.php", $( this ).serializeArray());
+    $('#loading-message').show();
+    $.post("../sendemail.php", $( this ).serializeArray())
+        .then(function(success){
+            $( this ).reset();
+            $('#success-message').show();
+            $('#loading-message').hide();
+        }, 
+        function(error){
+            $('#error-message').show();
+            $('#loading-message').hide();
+        });
     event.preventDefault();
 });
